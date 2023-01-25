f16a243d9b4b4b1ec584a3d96f06e567a431b9ac
Christian Ani <iamchristianani@gmail.com>
2023-01-25 14:32:59 +0100

    Update category file to use category action

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,21 @@
-const Categories = () => (
-  <div>
-    <button className="categoryButton" type="button">Check Status</button>
-  </div>
-);
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { categoryAction } from '../redux/categories/categories';
+
+const Categories = () => {
+  const dispatch = useDispatch();
+  const categories = useSelector((state) => state.categories);
+  const checkStatus = () => {
+    dispatch(categoryAction());
+  };
+
+  return (
+    <div>
+      <span>{categories}</span>
+      <br />
+      <button className="categoryButton" type="button" onClick={checkStatus}>Check Status</button>
+    </div>
+  );
+};
 
 export default Categories;
