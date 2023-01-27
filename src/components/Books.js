4d8557a3c3835c01ecc3dea562097724fc86d03a
Christian Ani <iamchristianani@gmail.com>
2023-01-27 11:00:24 +0100

    Import and use useDispatch and useEffect

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
+import { getBooksAction } from '../redux/books/books';
 
 const Books = () => {
+  const dispatch = useDispatch();
   const data = useSelector((state) => state.books);
+
+  useEffect(() => {
+    dispatch(getBooksAction());
+  }, [dispatch]);
+
   return (
     <>
       <ul className="allBooks">
         {
           data.map((book) => (
-            <li key={book.id}>
-              <Book id={book.id} title={book.title} author={book.author} />
+            <li key={book.bookId}>
+              <Book key={book.bookId} id={book.bookId} title={book.title} author={book.author} />
             </li>
           ))
         }
