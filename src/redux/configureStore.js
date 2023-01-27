0d3d56f96fd80d85a679dbcc64b530c6bd6e6593
Christian Ani <iamchristianani@gmail.com>
2023-01-27 10:59:10 +0100

    Import and use configureStore and applyMiddleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import categoryReducer from './categories/categories';
 import bookReducer from './books/books';
 
-const store = configureStore({
-  reducer: {
-    categories: categoryReducer,
-    books: bookReducer,
+const store = configureStore(
+  {
+    reducer: {
+      categories: categoryReducer,
+      books: bookReducer,
+    },
   },
-});
+  applyMiddleware(thunk),
+);
 
 export default store;
