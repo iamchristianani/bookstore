8769a7d6773dfb2267845794844fa2cc23a66a38
Christian Ani <iamchristianani@gmail.com>
2023-01-28 17:13:40 +0100

    Add category to the books page

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,14 +3,44 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeAction } from '../redux/books/books';
 
-const Book = ({ id, title, author }) => {
+const Book = ({
+  id, title, author, category,
+}) => {
   const dispatch = useDispatch();
 
   return (
     <div id={id} className="oneBook">
-      <h2>{title}</h2>
-      <p>{author}</p>
-      <button type="button" className="bookButton" onClick={() => dispatch(removeAction(id))}>Remove</button>
+      <div className="bookInfo">
+        <h4 className="bookCategory">{category}</h4>
+        <h2>{title}</h2>
+        <p className="bookAuthor">{author}</p>
+        <div className="btnActions">
+          <button type="button" className="eachActionBtn">Comments</button>
+          <button type="button" className="eachActionBtn" onClick={() => dispatch(removeAction(id))}>Remove</button>
+          <button type="button" className="eachActionBtn">Edit</button>
+        </div>
+      </div>
+      <div className="progressPart">
+        <div className="progressRate">
+          <div className="progressCircleContainer">
+            <div className="progressCircle">{null}</div>
+          </div>
+          <div className="progressStat">
+            <p className="statAmount">64%</p>
+            <p className="statCompleted">Completed</p>
+          </div>
+        </div>
+        <div className="progressDivider">{null}</div>
+        <div className="progressUpdate">
+          <div className="chapterPart">
+            <p className="chapterLabel">CURRENT CHAPTER</p>
+            <p className="mainChapter">Chapter 17</p>
+          </div>
+          <div className="updateBtnBox">
+            <button type="button" className="updateBtn">UPDATE PROGRESS</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -19,6 +49,7 @@ Book.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default Book;
